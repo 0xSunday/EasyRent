77d5948834b52a00035d336a60860ef1cef42f4d
Sunil Reddy <sunday7637@gmail.com>
2024-02-18 09:07:50 +0530

    done with graph ql

---
+++ Services/index.tsx
@@ -0,0 +1,27 @@
+import { request, gql } from "graphql-request";
+
+export const getVehicleList = async () => {
+  const document = gql`
+    query MyQuery {
+      vehicleS {
+        avarage
+        createdAt
+        engType
+        id
+        name
+        price
+        publishedAt
+        seat
+        stage
+        updatedAt
+        vehicleBrand
+        vehicleType
+      }
+    }
+  `;
+  const result = await request(
+    "https://api-ap-south-1.hygraph.com/v2/clsops88k059s01ume6p5ro2q/master",
+    document
+  );
+  return result;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+import { getVehicleList } from "@/Services";
 import FilterOptions from "@/components/FilterOptions";
 import Hero from "@/components/Hero";
 import SearchInput from "@/components/SearchInput";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [vehicles, setVehicles] = useState<any>([]);
+
+  useEffect(() => {
+    getVehicleList_();
+  }, []);
+
+  const getVehicleList_ = async () => {
+    const result = await getVehicleList();
+    setVehicles(result);
+  };
+  console.log(vehicles);
   return (
     <main className="overflow-hidden ">
       <Hero />
